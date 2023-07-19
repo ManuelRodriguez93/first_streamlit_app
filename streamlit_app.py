a9f2326e674d5276db4b4d97abe76fd800541d2b
ManuelRodriguez93 <122572192+ManuelRodriguez93@users.noreply.github.com>
2023-07-19 16:01:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,26 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
-  
-streamlit.header(' Breaksfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smootie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.title ('My Parents New Healthy Diner')
+
+streamlit.header ('breakfast favorites')
+streamlit.text (' 🥣 Omega 3 & blueberry Oatmeal')
+streamlit.text (' 🥗 Kale, Spinash & Rocket Smoothie')
+streamlit.text (' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text (' 🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 
-#create the repeteable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-    # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
-    return fruityvice_normalized
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
